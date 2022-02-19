2903b43c81ef3e021ff9a8da74ae4f43347aa5dc
HongJinHyeok <redmax45@naver.com>
2022-02-19 15:34:45 +0900

    connecting firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,50 +1,56 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class RandomWords extends StatefulWidget {
-  @override
-  _RandomWordsState createState() => _RandomWordsState();
-}
-
-class _RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final _biggerFont = const TextStyle(fontSize: 18);
-
-  Widget _buildSuggestions() {
-    return ListView.builder(
-      padding: const EdgeInsets.all(16),
-      itemBuilder: (context, i) {
-        if (i.isOdd) {
-          return const Divider();
-        }
-      }
-    )
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
-  }
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(title: const Text('Welcome to Flutter')),
-        body: Center(
-          child: RandomWords(),
+        title: 'Flutter Firebase Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
-      ),
-    );
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Firebase'),
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () => FirebaseFirestore.instance
+                .collection('testing')
+                .add({'timestamp': Timestamp.fromDate(DateTime.now())}),
+            child: Icon(Icons.add),
+          ),
+          body: StreamBuilder(
+            stream:
+                FirebaseFirestore.instance.collection('testing').snapshots(),
+            builder: (
+              BuildContext context,
+              AsyncSnapshot<QuerySnapshot> snapshot,
+            ) {
+              if (!snapshot.hasData) return const SizedBox.shrink();
+              return ListView.builder(
+                itemCount: snapshot.data!.docs.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final docData = snapshot.data!.docs[index];
+                  final dateTime =
+                      (docData['timestamp']! as Timestamp).toDate();
+                  return ListTile(
+                    title: Text(dateTime.toString()),
+                  );
+                },
+              );
+            },
+          ),
+        ));
   }
 }
